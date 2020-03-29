6ae69c1e444cc56bc0f9422960cdddb01aab93e0
Nathan Isaac <nathanjisaac@gmail.com>
2020-03-28 22:33:56 -0700

    Move toggles

--- components/NameRow.vue
+++ components/NameRow.vue
@@ -3,17 +3,19 @@
     <td
       class="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900"
     >
-      {{ name.fullName }}
+      <div class="flex">
+        <InputToggle :value="name.liked" @input="toggleLiked"></InputToggle>
+        <span class="inline-block ml-5 text-2xl">
+          {{ name.fullName }}
+        </span>
+      </div>
     </td>
     <td
       class="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium"
     >
-      <InputToggle :value="name.used" @input="toggleUsed"></InputToggle>
-    </td>
-    <td
-      class="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium"
-    >
-      <InputToggle :value="name.liked" @input="toggleLiked"></InputToggle>
+      <div class="flex justify-center">
+        <InputToggle :value="name.used" @input="toggleUsed"></InputToggle>
+      </div>
     </td>
   </tr>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -14,14 +14,9 @@
                   Name
                 </th>
                 <th
-                  class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                  class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
                 >
                   Used
-                </th>
-                <th
-                  class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Like/Dislike
                 </th>
               </tr>
             </thead>
