732666f79b6cd9da3b2630edac43540e905b40e7
Nathan Isaac <nathanjisaac@gmail.com>
2020-06-29 22:27:17 -0700

    Add example typescript mocha tests for electron

---
+++ test/example.test.ts
@@ -0,0 +1,9 @@
+import * as assert from "assert"
+
+describe('this', function () {
+  describe('#indexOf()', function () {
+    it('test something else', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
